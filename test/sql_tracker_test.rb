9da481fc3757dbcb7051b3e7e1493125155b21bc
Jason Dougherty <jasondoc3@gmail.com>
2020-03-05 15:08:41 -0800

    Support tracking queries in a block

---
+++ test/sql_tracker_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+module SqlTracker
+  class HandlerTest < Minitest::Test
+    def test_tracking_queries_with_a_block
+      config = SqlTracker::Config.apply_defaults
+      config.enabled = true
+      config.tracked_sql_command = %w(SELECT INSERT)
+
+      expected_queries = [
+        'SELECT * FROM users',
+        'insert into users VALUES (xxx)'
+      ]
+
+      query_data = SqlTracker.track do
+        expected_queries.each { |q| instrument_query(q) }
+        instrument_query('DELETE FROM users WHERE id = 1')
+      end
+
+      instrument_query('SELECT * FROM comments')
+
+      assert_equal(
+        query_data.values.map { |v| v[:sql] },
+        expected_queries
+      )
+    end
+
+    private
+
+    def instrument_query(query)
+      ActiveSupport::Notifications.instrument(
+        'sql.active_record',
+        sql: query
+      )
+    end
+  end
+end
